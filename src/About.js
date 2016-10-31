43d5d3a66af54ccb2f19a03620c11f1a3a04d685
Maksymilian Barnaś <max.barnas@gmail.com>
2016-11-01 00:06:30 +0100

    adding another page

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './About.css';
+
+class About extends Component {
+	render() {
+		return (
+			<div className="About-box">
+				This is the About part.
+			</div>
+		);
+	}
+}
+
+export default About;
